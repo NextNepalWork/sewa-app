552b66f4c1a881848120902e2ab373e6c08d9f25
Bipin Joshi <joshibipin2052@gmail.com>
2022-06-30 14:05:17 +0545

    a

--- android/app/src/main/kotlin/com/example/sewa_digital/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.customer.sewaexpress
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
--- lib/data/model/order_cancel_response.g.dart
+++
@@ -1,5 +0,0 @@
-part of 'order_cancel_response.dart';
-
-OrderCancelResponse _$OrderCancelResponseFromJson(Map<String, dynamic> json) =>
-    OrderCancelResponse(json['order_code'] as String?,
-        json['message'] as String?, json['success'] as bool?);
